e0c0ffea2674f6de7d22d083b1db7df8292337b3
Swufin <110313086+Swufin@users.noreply.github.com>
2026-07-08 03:51:45 -0700

    housekeeping + sync fix
    fixed sync issues caused by audio lag

--- project/scripts/website.gd
+++ project/scripts/website.gd
@@ -5,7 +5,6 @@ extends Node2D
 @onready var bloom: ColorRect = $Effects/Bloom
 
 var last_beat: int = -1
-
 var bloom_tween: Tween
 
 func _ready() -> void:
@@ -14,41 +13,49 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if !music.playing:
 		return
-		
+
 	var stream = music.stream
 	if stream == null or stream.bpm <= 0.0:
 		return
-	
+
 	var beat_length: float = 60.0 / stream.bpm
-	var beat = floori(music.get_playback_position() / beat_length)
-	
+	var song_time = get_song_time()
+	var beat = floori(song_time / beat_length)
+
 	if beat != last_beat:
 		last_beat = beat
-		
+
 		if beat % 2 == 0:
 			pulse_bloom()
 
+func get_song_time() -> float:
+	return (
+		music.get_playback_position()
+		+ AudioServer.get_time_since_last_mix()
+		- AudioServer.get_output_latency()
+	)
+
 func pulse_bloom() -> void:
-	var shader_material: ShaderMaterial = bloom.material
+	var shader_material = bloom.material as ShaderMaterial
 	if shader_material == null:
 		return
 
 	if bloom_tween:
 		bloom_tween.kill()
 
-	shader_material.set_shader_parameter("bloom_intensity", 0.5)
+	shader_material.set_shader_parameter("bloom_intensity", 0.3)
 
 	bloom_tween = create_tween()
 	bloom_tween.tween_method(
 		func(value: float):
 			shader_material.set_shader_parameter("bloom_intensity", value),
-		0.1,
+		0.3,
 		0.0,
 		0.5
 	)
 
 func _on_dialogue_finished() -> void:
 	last_beat = -1
-	
+
 	music.play()
 	player.play("intro")
